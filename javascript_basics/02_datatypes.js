5ff5c52a6e987070de9581ea96a52dd16fb32133
M-Zeeshan568 <142298879+M-Zeeshan568@users.noreply.github.com>
2023-11-27 20:31:59 +0000

    datatypes

--- javascript_basics/02_datatypes.js
+++ javascript_basics/02_datatypes.js
@@ -8,3 +8,46 @@
 // undefined
 // symbol
 // object
+
+
+
+
+// Reference or Non_Primitive_Datatypes
+
+//Arrays
+//Objects
+//Functions
+
+
+
+
+// Arthematic_Operators
+
+// 3 + 4   => Add
+// 5 - 7   => Substraction
+// 5 / 7   => Division
+// 5 * 7   => Multiplication
+// 5 ** 7  => Power
+// 5++     => Increament
+// 5--     => Decreament
+
+
+
+// Comparison_Operators
+// 5 = 7  => Equal to 
+// 5 < 7  => Less then
+// 5 > 7  => Greater then
+// 5 == 7 => Equal to
+// 5 === 7=> values and datatypes equal to
+// 5 ! 7  => Not equal to 
+// 5 !== 7=> values and datatypes not equal to
+
+
+//Concatenate_Values
+
+// console.log('1' + 2 + 7);
+// console.log(1 + 2 + '7');
+
+
+
+
