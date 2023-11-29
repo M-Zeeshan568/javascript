304b34d581d59a68a123557bb7f69ea331976d5f
M-Zeeshan568 <142298879+M-Zeeshan568@users.noreply.github.com>
2023-11-29 19:38:26 +0000

    Array methods

---
+++ javascript_basics/04_array.js
@@ -0,0 +1,58 @@
+
+const arry = [1, 2,3,4,5]
+
+// arry.push(7);
+// arry.pop();
+
+// arry.unshift(4);
+// arry.shift();
+
+// console.log(arry.includes(9));
+// console.log(arry.indexOf(9));
+
+// const newArry = arry.join()
+
+// console.log(arry);
+
+// slice()
+// splice()
+
+
+
+const marvel_heros = ['thor', 'captain america', 'zeeshan']
+
+const heros = ['akshay', 'govenda', 'heritak']
+
+// marvel_heros.push(heros);
+// const all_heros = marvel_heros.concat(heros);
+const all_new_heros = [...marvel_heros, ...heros]
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+// console.log(all_new_heros);
+
+const nestedArry =[1,2,3,[1,2,3],7,9,[4,5,6,[9,0,4]]]
+
+// console.log(nestedArry.flat(Infinity));
+// console.log(nestedArry);
+// console.log(nestedArry.flat(3));
+
+
+// console.log(Array.isArray('Zeeshan'));
+// console.log(Array.from('Zeeshan'));
+// console.log(Array.from({name: 'Zeeshan'}));
+
+
+
+let value1 = 300
+let value2 = 500
+let value3 = 700
+
+console.log(Array.of(value1, value2, value3));
+
+
+
+
+
+
+
